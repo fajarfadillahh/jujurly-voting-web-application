25cabb31fea406e66ff9ea46d2e03a604b9fd7d1
fajarfadillahh <fajaragustian979@gmail.com>
2023-08-05 00:46:49 +0700

    modifed voting rooms [code].jsx

--- pages/rooms/[code].jsx
+++ pages/rooms/[code].jsx
@@ -1,5 +1,9 @@
 import Head from "next/head";
 
+// import components
+import Layout from "@/components/Layout";
+import Button from "@/components/Button";
+
 export default function Voting() {
   return (
     <>
@@ -7,7 +11,51 @@ export default function Voting() {
         <title>Ayo, Tentukan pilihan kamu sekarang!</title>
       </Head>
 
-      <main>voting room</main>
+      <Layout>
+        <section className="pb-16 pt-32">
+          <div className="container grid justify-items-center gap-8">
+            <div className="text-center">
+              <h1 className="section-title">Pemilihan Ketua Osis</h1>
+
+              <div className="grid gap-2">
+                <p className="section-text">Voting akan berakhir pada :</p>
+                <div className="inline-flex items-center justify-center gap-5">
+                  <div className="grid justify-items-center">
+                    <span className="-mb-2 text-[48px] font-bold text-black">
+                      26
+                    </span>
+                    <span className="text-[14px] font-medium text-black">
+                      Jam
+                    </span>
+                  </div>
+                  <span className="text-[32px] font-medium">:</span>
+                  <div className="grid justify-items-center">
+                    <span className="-mb-2 text-[48px] font-bold text-black">
+                      42
+                    </span>
+                    <span className="text-[14px] font-medium text-black">
+                      Menit
+                    </span>
+                  </div>
+                  <span className="text-[32px] font-medium">:</span>
+                  <div className="grid justify-items-center">
+                    <span className="-mb-2 text-[48px] font-bold text-black">
+                      26
+                    </span>
+                    <span className="text-[14px] font-medium text-black">
+                      Detik
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div>candidate</div>
+
+            <Button text="Kirim Voting 🚀" variant="fill" className="mt-8" />
+          </div>
+        </section>
+      </Layout>
     </>
   );
 }
