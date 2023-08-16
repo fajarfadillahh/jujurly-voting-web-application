7bea901fe0a17a92c763dae24e0bc4453f6f44db
gufronnakaaw <gufronnakaaw@gmail.com>
2023-08-16 12:01:09 +0700

    modified date in convert time

--- utils/convert.js
+++ utils/convert.js
@@ -1,13 +1,22 @@
 export function convertTime(time) {
-  const days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
-  const date = new Date(time);
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDay();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
+  const days = [
+    "Minggu",
+    "Senin",
+    "Selasa",
+    "Rabu",
+    "Kamis",
+    "Jum'at",
+    "Sabtu",
+  ];
+  const dateInstance = new Date(time);
+  const year = dateInstance.getFullYear();
+  const month = dateInstance.getMonth() + 1;
+  const day = dateInstance.getDay();
+  const date = dateInstance.getDate();
+  const hours = dateInstance.getHours();
+  const minutes = dateInstance.getMinutes();
 
-  return `${days[day]} ${day < 10 ? `0${day}` : day}/${
+  return `${days[day]} ${date < 10 ? `0${date}` : date}/${
     month < 10 ? `0${month}` : month
   }/${year} ${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
